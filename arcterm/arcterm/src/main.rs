331f2c828ce0b7c8d4402d59f8a08cdb68188255
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-28 17:46:06 +1100

    Updates. idk how text works in iced

--- arcterm/arcterm/src/main.rs
+++ arcterm/arcterm/src/main.rs
@@ -1,5 +1,10 @@
+extern crate std;
+use iced::Text;
+
 fn main() {
     // uses rust-std library
     // writes to the screen line by line, up to the driver to display it properly
     // uses black font by default, cant change font size and colors through "userspace module" Display::FontAPI
+
+
 }
---
+++ arcterm/arcterm/tests/arcterm.rs
@@ -0,0 +1,10 @@
+#[test]
+fn test_basics() {
+    // open an iced-rs window
+
+    // render the console with an expanding text box and vertically scrollable view
+    // no horizontal scroll, lines are wrapped at 80 characters. Encourage use of new lines
+
+    // make background color black for the console view area and the entire window
+
+}
