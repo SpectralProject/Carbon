f80b8309f506bb4bdf049ab9aa887c82bf0c7ba9
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-03-05 15:55:39 +1100

    ADD

--- userspace/src/icons.rs
+++ userspace/src/icons.rs
@@ -17,3 +17,16 @@ return FFX`
 </Box>
 `
 */
+
+// Rei Language server with sockets
+// Open an RPC socket. Bind the address/pid of the running RPC server on localhost:<port>
+// get the socket_descriptor of the RPC socket and send/receive JSON strings through reads/writes to the socket
+// would usually sit in a constant loop waiting for a queued request and accept()
+// if a change detected in your code, would make a send()
+
+// Rei LSP with gRPC
+// request-reply on UDP for lower overhead, esp on local machines where errors are much rarer
+// Start the Language server reiserver on <pid>
+// In the IDE, open a channel communication pipe with the process directly (IPC) -> pipe_open(pid)
+// 
+
